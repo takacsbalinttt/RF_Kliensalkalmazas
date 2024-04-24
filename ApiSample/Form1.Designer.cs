863858132230501188ef778e91617a589df4245b
kalocziistvan <kalocziisti@gmail.com>
2024-04-24 11:08:09 +0200

    kezdetleges design, de még dolgozom rajta

--- ApiSample/Form1.Designer.cs
+++ ApiSample/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button_mentes = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox_mennyiseg = new System.Windows.Forms.TextBox();
@@ -41,98 +42,100 @@
             this.nev = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.keszlet = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.inventory_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.termekBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.termekBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // button_mentes
             // 
-            this.button_mentes.Location = new System.Drawing.Point(1642, 440);
-            this.button_mentes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_mentes.BackColor = System.Drawing.Color.Yellow;
+            this.button_mentes.Location = new System.Drawing.Point(1124, 351);
             this.button_mentes.Name = "button_mentes";
-            this.button_mentes.Size = new System.Drawing.Size(136, 125);
+            this.button_mentes.Size = new System.Drawing.Size(91, 80);
             this.button_mentes.TabIndex = 1;
             this.button_mentes.Text = "Kész";
-            this.button_mentes.UseVisualStyleBackColor = true;
+            this.button_mentes.UseVisualStyleBackColor = false;
             this.button_mentes.Click += new System.EventHandler(this.button_mentes_Click);
             // 
             // listBox1
             // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.MenuText;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(33, 103);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(22, 127);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(406, 629);
+            this.listBox1.Size = new System.Drawing.Size(272, 404);
             this.listBox1.TabIndex = 3;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // textBox_mennyiseg
             // 
-            this.textBox_mennyiseg.Location = new System.Drawing.Point(1482, 252);
-            this.textBox_mennyiseg.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox_mennyiseg.Location = new System.Drawing.Point(1017, 233);
             this.textBox_mennyiseg.Name = "textBox_mennyiseg";
-            this.textBox_mennyiseg.Size = new System.Drawing.Size(295, 31);
+            this.textBox_mennyiseg.Size = new System.Drawing.Size(198, 22);
             this.textBox_mennyiseg.TabIndex = 4;
             // 
             // textBox_kereses
             // 
-            this.textBox_kereses.Location = new System.Drawing.Point(33, 42);
-            this.textBox_kereses.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox_kereses.Location = new System.Drawing.Point(22, 99);
             this.textBox_kereses.Name = "textBox_kereses";
-            this.textBox_kereses.Size = new System.Drawing.Size(406, 31);
+            this.textBox_kereses.Size = new System.Drawing.Size(272, 22);
             this.textBox_kereses.TabIndex = 5;
             this.textBox_kereses.TextChanged += new System.EventHandler(this.textBox_kereses_TextChanged);
             // 
             // button_plus
             // 
-            this.button_plus.Location = new System.Drawing.Point(1642, 331);
-            this.button_plus.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_plus.BackColor = System.Drawing.Color.Yellow;
+            this.button_plus.Location = new System.Drawing.Point(1124, 277);
             this.button_plus.Name = "button_plus";
-            this.button_plus.Size = new System.Drawing.Size(90, 78);
+            this.button_plus.Size = new System.Drawing.Size(60, 50);
             this.button_plus.TabIndex = 6;
             this.button_plus.Text = "+";
-            this.button_plus.UseVisualStyleBackColor = true;
+            this.button_plus.UseVisualStyleBackColor = false;
             this.button_plus.Click += new System.EventHandler(this.button_plus_Click);
             // 
             // button_minus
             // 
-            this.button_minus.Location = new System.Drawing.Point(1526, 331);
-            this.button_minus.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_minus.BackColor = System.Drawing.Color.Yellow;
+            this.button_minus.Location = new System.Drawing.Point(1048, 277);
             this.button_minus.Name = "button_minus";
-            this.button_minus.Size = new System.Drawing.Size(90, 78);
+            this.button_minus.Size = new System.Drawing.Size(60, 50);
             this.button_minus.TabIndex = 7;
             this.button_minus.Text = "-";
-            this.button_minus.UseVisualStyleBackColor = true;
+            this.button_minus.UseVisualStyleBackColor = false;
             this.button_minus.Click += new System.EventHandler(this.button_minus_Click);
             // 
             // button_megse
             // 
-            this.button_megse.Location = new System.Drawing.Point(1480, 440);
-            this.button_megse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_megse.BackColor = System.Drawing.Color.Yellow;
+            this.button_megse.Location = new System.Drawing.Point(1017, 351);
             this.button_megse.Name = "button_megse";
-            this.button_megse.Size = new System.Drawing.Size(136, 125);
+            this.button_megse.Size = new System.Drawing.Size(91, 80);
             this.button_megse.TabIndex = 8;
             this.button_megse.Text = "Mégse";
-            this.button_megse.UseVisualStyleBackColor = true;
+            this.button_megse.UseVisualStyleBackColor = false;
             this.button_megse.Click += new System.EventHandler(this.button_megse_Click);
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Id,
             this.nev,
             this.keszlet,
             this.inventory_id});
-            this.dataGridView1.Location = new System.Drawing.Point(446, 103);
+            this.dataGridView1.Location = new System.Drawing.Point(297, 127);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 82;
             this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(1027, 629);
+            this.dataGridView1.Size = new System.Drawing.Size(685, 403);
             this.dataGridView1.TabIndex = 9;
-            
             // 
             // Id
             // 
@@ -162,15 +165,27 @@
             this.inventory_id.Name = "inventory_id";
             this.inventory_id.Width = 200;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(423, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 73);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
             // termekBindingSource
             // 
             this.termekBindingSource.DataSource = typeof(ApiSample.Termek);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1825, 767);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1242, 543);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button_megse);
             this.Controls.Add(this.button_minus);
@@ -179,10 +194,10 @@
             this.Controls.Add(this.textBox_mennyiseg);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button_mentes);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.termekBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -203,5 +218,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn nev;
         private System.Windows.Forms.DataGridViewTextBoxColumn keszlet;
         private System.Windows.Forms.DataGridViewTextBoxColumn inventory_id;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
